2e95f04599431e73745440a4139c87f67ca5b59f
Petre Vane <petre.vane@gmail.com>
2024-08-05 12:15:24 +0200

    Rename SNS topic

--- sns/main.tf
+++ sns/main.tf
@@ -1,11 +1,11 @@
 
 // ssn topic for publishing failed lambda executions
-resource "aws_sns_topic" "lambda_failures" {
-  name = "lambda_failures"
+resource "aws_sns_topic" "lambda_execution_results" {
+  name = "lambda_execution_results"
 }
 
 resource "aws_sns_topic_subscription" "email_subscription" {
-  topic_arn = aws_sns_topic.lambda_failures.arn
+  topic_arn = aws_sns_topic.lambda_execution_results.arn
   protocol  = "email"
   endpoint  = var.notification_email // Email for notifications
 }
